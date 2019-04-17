3e6881c594bb65d4323588f66ca294c39ce29e55
RunningLeon <mnsheng@yeah.net>
2019-04-17 23:37:41 +0800

    add scripts of HumanHeart_SingleCell_Cui2019 MouseBrain_SingleCell_YeEmilyWu2017

---
+++ scripts/MouseBrain_SingleCell_YeEmilyWu2017/run.sh
@@ -0,0 +1,18 @@
+bash_dir=$(cd `dirname $0`; pwd)
+proj_dir=$bash_dir/../../
+
+# input="/home/disk/yangjing/tianlab/tools/scRef-master/Reference/yj31_38/4th_human/matrix_dealing/HumanPluripotentStemCell_SingleCell_Han2018"
+data_dir=$bash_dir
+input=$data_dir/GSE103976_MeA_AllCells_DGE.txt
+meta_path=$data_dir/cell_meta.txt
+
+cell_typ_path="$data_dir/cell_type.txt"
+# key_name="Cell type"
+output_dir=$bash_dir/output
+
+python $proj_dir/src/create_raw_sum.py \
+    --input $input \
+    --cell-meta $meta_path \
+    --output $output_dir \
+    --cell-typ $cell_typ_path \
+    # --key-name $key_name \
