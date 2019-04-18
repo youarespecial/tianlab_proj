86e8dd5fcd2d3b0cf6c5ececadbd27ecb57ab8b9
RunningLeon <mnsheng@yeah.net>
2019-04-18 13:26:07 +0800

    update

--- scripts/HumanPluripotentStemCell_SingleCell_Han2018/run.sh
+++ scripts/HumanPluripotentStemCell_SingleCell_Han2018/run.sh
@@ -1,17 +1,18 @@
 bash_dir=$(cd `dirname $0`; pwd)
+proj_dir=$bash_dir/../..
 
 
 # data_dir="/home/disk/yangjing/tianlab/tools/scRef-master/Reference/yj31_38/4th_human/matrix_dealing/HumanPluripotentStemCell_SingleCell_Han2018"
 data_dir=$bash_dir
-annot_path=$data_dir/anno_all.csv
+anno_path=$data_dir/anno_all.csv
 
-celltype_path=$data_dir/cell_type.txt
-
+celltype_path="$data_dir/cell_type.txt"
+# key_name="Cell type"
 output_dir=$bash_dir/output
-# src_dir=$bash_dir/../../src
 
 python $bash_dir/process.py \
-	--input $data_dir \
-	--output $output_dir \
-	--anno-all $annot_path \
-	--cell-type $celltype_path \
+    --input $data_dir \
+    --output $output_dir \
+    --anno $anno_path \
+    --cell-typ $cell_typ_path \
+    # --key-name $key_name \
